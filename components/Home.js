4d93540e84e4bd71af0aff8da9f027d24ac8c175
Anna2493 <adawidowska24@gmail.com>
2021-09-16 12:29:23 +0100

    added interval timer

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Alert, ScrollView, Button, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Alert, ScrollView, Button, SafeAreaView, TextInput, TouchableNativeFeedbackBase } from 'react-native';
 
 let padToTwo = (number) => (number <= 9 ? `0${number}`: number);
 
@@ -7,68 +7,157 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            start: false,
             min: 0,
             sec: 0,
             msec: 0,
-            contractions: [],
+            contractionsData: [],
+
+            intervalMin: 0,
+            intervalSec: 0,
+            intervals: [],
         }
 
+        this.contraction = null;
         this.interval = null;
     }
 
     handleStartBtn = () => {
         //Update start button 
-        this.setState({ start : !this.state.start}, () => this.handleStart())
+       // this.setState({ start : !this.state.start}, () => this.switchStatement())
+        this.setState(prevState => ({
+            start: !prevState.start
+        }), ()=>this.switchStatement(this.state.start));
+
+        //console.log(this.state.start)
+
+       // this.switchStatement()
     }
 
-    handleStart = () => {
-        //Start timer
-        if (this.state.start) {
-            this.interval = setInterval(() => { //setInterval allows to use milliseconds to time response
-                //1 second = 100 milliseconds 
-                //1 minute = 60 seconds
-                if (this.state.msec !== 99) {
-                    this.setState({
-                        msec: this.state.msec + 1
-                    });
-                } else if (this.state.sec !== 59) {
-                    this.setState({
-                        msec: 0,
-                        sec: ++this.state.sec
-                    });
+    switchStatement = (start) => {
+        switch (this.state.start) {
+            case true:
+                this.handleContraction();
+                break;
+            case false:
+                this.handleInterval();
+        }
+
+        this.addData();
+    }
+
+    handleContraction = () => {
+        //---CLEAR INTERVAL TIMER---
+        this.setState({
+            intervalMin: 0,
+            intervalSec: 0,
+            start: true
+        });
+        clearInterval(this.interval);
+        
+        //---START CONTRACTION TIMER---
+        this.contraction = setInterval(() => { 
+            if (this.state.sec !== 59) {
+                this.setState({ sec: ++this.state.sec });
                 } else {
                     this.setState({
-                        msec: 0,
                         sec: 0,
                         min: ++this.state.min
                     });
                 }
-            }, 1);
+        }, 1000);
+        
+    //     //---ADD CONTRACTION TO THE ARRAY---
 
-        } else {
-            //Add contraction to the list
+    //    // if (this.state.start === false) {
+    //         //make a copy of the array
+    //         var copy = this.state.contractions
             
+    //         //get curent time
+    //         var hours = new Date().getHours();
+    //         var min = new Date().getMinutes();
+            
+    //         //formulate contraction data
+    //         copy.push({ time: hours + ':' + min, length: padToTwo(this.state.min) + ':' + padToTwo(this.state.sec) })
+            
+    //         //update the array with new contraction
+    //         this.setState({ contractions: copy })
+    //  //   }
+    }
+
+    handleInterval = () => {
+        //---CLEAR CONTRACTION TIMER---
+        this.setState({
+            min: 0,
+            sec: 0,
+            start: false
+        });
+        clearInterval(this.contraction);
+        
+        //---START INTERVAL TIMER---
+        this.interval = setInterval(() => {
+
+            if (this.state.intervalSec !== 59) {
+                this.setState({ intervalSec: ++this.state.intervalSec })
+            } else {
+                this.setState({
+                    intervalSec: 0,
+                    intervalMin: ++this.state.intervalMin
+                })
+            }
+        }, 1000);
+        
+    //     //---ADD INTERVAL TO THE ARRAY---
+
+    //    // if (this.state.start === true) {
+    //         //make a copy of the array
+    //         var copy = this.state.intervals
+            
+    //         //get curent time
+    //         var hours = new Date().getHours();
+    //         var min = new Date().getMinutes();
+            
+    //         //formulate interval data
+    //         copy.push({ time: hours + ':' + min, length: padToTwo(this.state.intervalMin) + ':' + padToTwo(this.state.intervalSec) })
+            
+    //         //update the array with new interval
+    //         this.setState({ intervals: copy })
+    //    // }
+    }
+
+    addData = () => {
+        //---ADD CONTRACTION TO THE ARRAY---
+
+       if (!this.state.start) {
+        //make a copy of the array
+        var copy = this.state.contractionsData
+        //get curent time
+        var hours = new Date().getHours();
+        var min = new Date().getMinutes();
+        //formulate contraction data
+           copy.push({ contractions: [{ time: hours + ':' + min, length: padToTwo(this.state.min) + ':' + padToTwo(this.state.sec) }] })
+        //update the array with new contraction
+        this.setState({ contractionsData: copy })
+        }
+
+        //---ADD INTERVAL TO THE ARRAY---
+
+       else{
             //make a copy of the array
-            var copy = this.state.contractions
+            var copy = this.state.contractionsData
             
-            //get currenmt time
+            //get curent time
             var hours = new Date().getHours();
             var min = new Date().getMinutes();
             
-            //formulate contraction data
-            copy.push({ time: hours+':'+min, length: this.state.min+':'+this.state.sec })
+            //formulate interval data
+           copy.push({ intervals: [{ time: hours + ':' + min, length: padToTwo(this.state.intervalMin) + ':' + padToTwo(this.state.intervalSec) }] })
             
-            //update the array with new contraction
-            this.setState({ contractions: copy })
-            //reset the timer
-            this.setState({
-                min: 0,
-                sec: 0,
-                msec: 0,
-                start: false
-            });
-            clearInterval(this.interval);
+            //update the array with new interval
+            this.setState({ contractionsData: copy })
         }
+        
+        console.log(this.state.contractionsData)
     }
 
     render() {
@@ -83,20 +172,38 @@ export default class Home extends Component {
 
                 <View style={styles.timeContainer}>
                     <Text style={styles.time}>{padToTwo(this.state.min) + ' : '}</Text>
-                    <Text style={styles.time}>{padToTwo(this.state.sec) + ' : '}</Text>
-                    <Text style={styles.time}>{padToTwo(this.state.msec)}</Text>
+                    <Text style={styles.time}>{padToTwo(this.state.sec)}</Text>
                 </View>
 
-                <ScrollView style={styles.listContainer}>
+                <View style={styles.timeContainer}>
+                    <Text style={styles.time}>{padToTwo(this.state.intervalMin) + ' : '}</Text>
+                    <Text style={styles.time}>{padToTwo(this.state.intervalSec)}</Text>
+                </View>
+
+                {/* <ScrollView style={styles.listContainer}>
                     <FlatList
-                        data={this.state.contractions}
-                        inverted={true}
+                        data={this.state.contractionsData}
+                        // inverted={true}
                         renderItem={({ item, index }) =>
                             <Text style={styles.contractionData} key={index}>
-                                {`${index + 1}`}        {item.time}     {item.length}
+                                {'Contraction'}        {item.time}     {item.length}
                             </Text>}
                     />
+                </ScrollView> */}
+
+                <ScrollView style={styles.listContainer}>
+                    {this.state.contractionsData.map((item) => {
+                        <View>
+                            {item.map((contraction, index) => {
+                                <View>
+                                    <Text key={index}>{contraction.time}    {contraction.length}</Text>
+                                </View>
+                            })}
+                        </View>
+                    })}
                 </ScrollView>
+
+               
             </View>
         )
     }
@@ -154,7 +261,7 @@ const styles = StyleSheet.create({
     },
 
     contractionData: {
-        fontSize: 30,
+        fontSize: 20,
         textAlign: 'center',
         backgroundColor: '#fff',
         marginBottom: 20,
